8ba59bebbc12e035f663d220b4ad54e491ca3ec1
hymndrazill <srihafiras@yahoo.fr>
2023-02-16 22:44:22 +0100

    gigs page and styling

--- fiverClone/src/pages/gigs/Gigs.jsx
+++ fiverClone/src/pages/gigs/Gigs.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./gigs.scss"
 const Gigs = () => {
+
+  const [open, setOpen] = useState(false)
+  const [sort, setSort] = useState("sales")
+
+  const reSort = (type) => {
+    setSort(type)
+    setOpen(false)
+  }
+
   return (
-    <div>
-      GIGS
+    <div className='gigs'>
+      <div className="container">
+          <span className='breadcrumbs'>FIVERR>  Graphics & DESIGN.</span>
+          <h1>AI Artists</h1>
+          <p>
+          explore the boundaries of art and technology with Fiverr's AI artists
+          </p>
+         <div className="menu">
+            <div className="left">
+              <span>Budget</span>
+              <input type="text" placeholder='Min' />
+              <input type="text" placeholder='Max' />
+              <button>Apply</button>
+            </div>
+            <div className="right">
+              <span className='sortby'>Sort By</span>
+              <span className='sorttype'>{ sort ==="sales" ? " Best Selling" : "Newest"}</span>
+              <img src="./img/down.png" alt=""  onClick={()=> setOpen(!open)} />
+             { open && 
+             <div className="rightmenu" >
+                { sort === "sales" ? (
+                  <span onClick={()=>reSort("createAt")}>Newest</span>
+                )
+                  : (
+                    <span onClick={()=>reSort("sales")}>Best Selling</span>
+                  )
+                }
+              </div>
+              }
+            </div>
+         </div>
+      </div>
     </div>
   )
 }
