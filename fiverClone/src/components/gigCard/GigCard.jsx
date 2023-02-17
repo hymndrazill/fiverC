3a654054d3dec10253c1ae83955738da55cf5c3b
hymndrazill <srihafiras@yahoo.fr>
2023-02-17 12:38:10 +0100

    finishing the gig component

--- fiverClone/src/components/gigCard/GigCard.jsx
+++ fiverClone/src/components/gigCard/GigCard.jsx
@@ -1,33 +1,40 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import "./gigcard.scss"
+
 const GigCard = ({item}) => {
   return (
-    <Link to="/gig/12">
-    <div className='gigcard'>
-        <img src={item.img} alt="" />
-        <div className="info"></div>
-            <div className="user">
-                <img src={item.pp} alt="" />
-                <span>{item.username}</span>
-            </div>
-            <p>{item.desc}</p>
-            <div className="star">
-                <img src="./img/star.png" alt="" />
-                <span>{item.star}</span>
-            </div>
-            <hr/>
-        <div className="details">
-            <img src="./img/heart.png" alt="" />
-            <div className="price">
-                <span>Starting at</span>
-                <h2>${item.price}</h2>
-            </div>
-
+    <Link to="/gig/123" className="link">
+    <div className="gigCard">
+      <img src={item.img} alt="" />
+      <div className="info">
+        <div className="user">
+          <img src={item.pp} alt="" />
+          <span>{item.username}</span>
         </div>
+        <p>{item.desc}</p>
+        <div className="star">
+          <img src="./img/star.png" alt="" />
+          <span>{item.star}</span>
+        </div>
+      </div>
+      <hr />
+      <div className="details">
+        <img src="./img/heart.png" alt="" />
+        <div className="price">
+          <span>STARTING AT</span>
+          <h2>
+            $ {item.price}
+            <sup>99</sup>
+          </h2>
+        </div>
+      </div>
     </div>
-    </Link>
-  )
-}
+  </Link>
+);
+};
+
+  
+
 
 export default GigCard
