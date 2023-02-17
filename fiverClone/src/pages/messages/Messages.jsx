8dc07abec795c2452643bbaeb4a0e4a3d1cb9dd8
hymndrazill <srihafiras@yahoo.fr>
2023-02-17 12:39:51 +0100

    wrapping up the messages page

--- fiverClone/src/pages/messages/Messages.jsx
+++ fiverClone/src/pages/messages/Messages.jsx
@@ -1,10 +1,84 @@
 import React from "react"
 import "./messages.scss"
-
+import {Link } from "react-router-dom"
 const Messages = () => {
+  const message = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam voluptatum corporis provident reiciendis sunt! Consectetur tempora ea esse excepturi inventore?`
   return (
-    <div>
-      messages
+    <div className="messages">
+    <div className="container">
+        <div className="title">
+          <h1>Messages</h1>
+          
+          </div>
+
+          <table>
+            <tr>
+              <th>Buyer</th>
+              <th>Last Message</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+            <tr className="active">
+              <td>
+             John Doe
+              </td>
+              <td>
+              <Link to="/message/12" className="link">
+              {message.substring(0,100)}...
+              </Link></td>
+              <td>1 Day Ago</td>
+              <td>
+                <button>Mark As Read</button>
+              </td>
+            </tr>
+            <tr className="active">
+              <td>
+             John Doe
+              </td>
+              <td>
+              <Link to="/message/12" className="link">
+              {message.substring(0,100)}...
+              </Link></td>
+              <td>1 Day Ago</td>
+              <td>
+                <button>Mark As Read</button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+             John Doe
+              </td>
+              <td>
+              <Link to="/message/12" className="link">
+              {message.substring(0,100)}...
+              </Link></td>
+              <td>1 Day Ago</td>
+
+            </tr>
+            <tr>
+              <td>
+             John Doe
+              </td>
+              <td>
+              <Link to="/message/12" className="link">
+              {message.substring(0,100)}...
+              </Link></td>
+              <td>1 Day Ago</td>
+             
+            </tr>
+            <tr>
+              <td>
+             John Doe
+              </td>
+              <td>
+              <Link to="/message/12" className="link">
+              {message.substring(0,100)}...
+              </Link></td>
+              <td>1 Day Ago</td>
+              
+            </tr>
+          </table>
+      </div>
     </div>
   )
 }
