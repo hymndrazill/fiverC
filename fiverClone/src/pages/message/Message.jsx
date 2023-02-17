d39f2b5ca9b256f6cbc94cf50f6e2425857881f8
hymndrazill <srihafiras@yahoo.fr>
2023-02-17 12:40:32 +0100

    starting up the message page

--- fiverClone/src/pages/message/Message.jsx
+++ fiverClone/src/pages/message/Message.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import {Link} from "react-router-dom"
 import "./message.scss"
 
 const Message = () => {
   return (
-    <div>
-      message
+    <div className="message">
+      <div className="container">
+        <span className="breadcrumbs"><Link to="/messages">MESSAGES</Link> > JOHN DOE></span>
+      </div>
     </div>
   )
 }
